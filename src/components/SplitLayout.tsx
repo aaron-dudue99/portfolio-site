ec4297df2a4a25e1b15f266404988d4aac1508ce
Aaron Dudue <duduzilesibanda99@gmail.com>
2026-02-02 22:08:44 +0200

    feat: increase max width of SplitLayout component for better responsiveness

--- src/components/SplitLayout.tsx
+++ src/components/SplitLayout.tsx
@@ -8,15 +8,13 @@ export default function SplitLayout({
   rightContent,
 }: SplitLayoutProps) {
   return (
-    <div className="mx-auto min-h-screen max-w-[1200px] px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+    <div className="mx-auto min-h-screen max-w-[1400px] px-6 py-12 font-sans md:py-20 lg:py-0">
       <div className="lg:flex lg:justify-between lg:gap-4 lg:items-start">
         <div className="lg:sticky lg:top-0 lg:flex lg:h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
           {leftContent}
         </div>
 
-        <div className="pt-24 lg:w-1/2 lg:py-24">
-          {rightContent}
-        </div>
+        <div className="pt-24 lg:w-1/2 lg:py-24">{rightContent}</div>
       </div>
     </div>
   );
